b487f50b042e8ea9de49560b57735d7781b26ce0
erciyes53 <v.unal@maraskaski.gov.tr>
2021-05-10 15:03:17 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(10,150))
+            {
+                Console.WriteLine("Ürün Adı="+product.ProductName+" Fiyat="+product.UnitPrice+" Stok Adedi="+product.UnitsInStock);
+            }
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //T için class verdik o referans tip demek
+    //IEntity demek ya IEntity nesnesi yada onu implemente eden bir nesne olabilir demek
+    //new demk newlenebilir demektit
+  public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        //filter=null ise filtre vermeyebilrim demek
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+
+        //filter böyle bırakılırsa filtre alacak demektir kesin
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
