d17aa1dfbd1790357f66075cbf5b57e0c2aad6b5
erciyes53 <v.unal@maraskaski.gov.tr>
2021-05-20 14:12:57 +0300

    20 mayıs ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -26,12 +26,21 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if (result.Success==true) 
             {
-                Console.WriteLine(" Ürün Adı=" + product.ProductName +
-                                  " Stok Adedi=" + product.UnitsInStock+
-                                  " Kategori Adı="+product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(" Ürün Adı=" + product.ProductName +
+                                      " Stok Adedi=" + product.UnitsInStock +
+                                      " Kategori Adı=" + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            
         }
     }
 }
